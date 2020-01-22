4f46af6596c54c50908c90c0e8f38855b2c223f9
ASIF MIMI <asif.mimi1@gmail.com>
2020-01-22 19:24:51 +0600

    Initial Commit

---
+++ Class,properties,Inheritance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Class,properties,Inheritance
+//
+//  Created by Asif Rabbi on 22/1/20.
+//  Copyright © 2020 Asif Rabbi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
